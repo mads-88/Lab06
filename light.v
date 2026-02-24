cf1003fc0a5783ab05fd5ed0881d7b35407819e0
mads-88 <madisonhickey@u.boisestate.edu>
2026-02-24 16:43:42 -0700

    Update light.v

--- light.v
+++ light.v
@@ -1,8 +1,14 @@
 module light(
     // Declare downstairs and upstairs input
     // Declare stair light output
+    input  wire downstairs,
+    input  wire upstairs,
+    output wire stair_light
+
 );
 
     // Enter logic equation here
+    assign stair_light = downstairs ^ upstairs;
+
 
 endmodule

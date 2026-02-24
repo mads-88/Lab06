ffea6d6b8d794de97387ccce06368fd6ba564d4b
mads-88 <madisonhickey@u.boisestate.edu>
2026-02-24 16:45:07 -0700

    Update full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -1,1 +1,13 @@
 // Implement module called full_adder
+module full_adder(
+    input  wire A,
+    input  wire B,
+    input  wire Cin,
+    output wire Y,
+    output wire Cout
+);
+
+    assign Y    = A ^ B ^ Cin;
+    assign Cout = (A & B) | (A & Cin) | (B & Cin);
+
+endmodule
